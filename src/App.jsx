082ae30f2d9fe61fd8f8f26424fe482aecb2703f
Taufik-H <taufikrpl1@gmail.com>
2023-06-28 13:10:20 +0700

    list

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,22 @@ import { fetchKartuData } from './api';
 import KartuDetail from './components/KartuDetail';
 import './index.css';
 
+const DaftarKartu = ({ kartuData }) => {
+  return (
+    <div>
+      <h1>Daftar Kartu</h1>
+      <ul>
+        {kartuData &&
+          Object.keys(kartuData).map((key) => (
+            <li key={key}>
+              <Link to={`/kartu/${key}`}>{key}</Link>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+};
+
 const App = () => {
   const [kartuData, setKartuData] = useState(null);
 
@@ -21,7 +37,7 @@ const App = () => {
     <Router>
       <div>
         <Routes>
-          {/* <Route path="/" element={<DaftarKartu kartuData={kartuData} />} /> */}
+          <Route path="/" element={<DaftarKartu kartuData={kartuData} />} />
           <Route
             path="/kartu/:key"
             element={<KartuDetail kartuData={kartuData} />}

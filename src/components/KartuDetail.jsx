02258e77aaea6d011760e0e45e820f3233b8d2a4
Taufik-H <taufikrpl1@gmail.com>
2023-06-29 01:09:09 +0700

    card animation

--- src/components/KartuDetail.jsx
+++ src/components/KartuDetail.jsx
@@ -1,19 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { motion } from 'framer-motion';
 import { bg } from '../assets';
+
 const KartuDetail = ({ kartuData }) => {
   const { key } = useParams();
   const kartu = kartuData ? kartuData[key] : null;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleCardClick = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
-    <div>
-      <img src={bg} alt="" className="object-cover w-full h-screen absolute" />
-      <div className="flex justify-center items-center h-screen relative">
-        {kartu ? (
-          <div className=" absolute w-10/12 md:w-3/12 mx-auto overflow-hidden shadow-md bg-white rounded-lg border">
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
+      <div className="flex justify-center items-center h-screen relative bg-gradient-to-t from-blue-500 to-blue-300">
+        <motion.div
+          whileTap={{ scale: 0.8 }}
+          onClick={handleCardClick}
+          className={`w-10/12 h-[400px] card-surprise rounded-md shadow-md bg-white overflow-hidden`}
+        >
+          <div className="w-full h-full">
+            <img src={bg} alt="" className="object-cover w-full h-full" />
+          </div>
+        </motion.div>
+        {kartu && (
+          <motion.div
+            onClick={handleCardClick}
+            className={`absolute w-11/12 md:w-3/12 mx-auto overflow-hidden shadow-md bg-white rounded-lg border ${
+              isOpen ? 'block' : 'hidden'
+            }`}
+            initial={{ scale: 0, opacity: 0 }}
+            animate={{ scale: isOpen ? 1 : 0, opacity: isOpen ? 1 : 0 }}
+            transition={{ duration: 0.2 }}
+          >
             <div className="relative">
               <img
-                className="w-full h-52  object-cover "
+                className="w-full h-52 object-cover"
                 src={kartu.gambar}
                 alt={kartu.subject}
               />
@@ -36,14 +63,10 @@ const KartuDetail = ({ kartuData }) => {
                 Pengirim: {kartu.username}
               </p>
             </div>
-          </div>
-        ) : (
-          <p className="p-10 rounded-md bg-white w-5/12 md:w-3/12 flex justify-center">
-            Loading
-          </p>
+          </motion.div>
         )}
       </div>
-    </div>
+    </motion.div>
   );
 };
 
